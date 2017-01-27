45b1c67c1de04c862206376944be44b84d818e30
Ilich García <ilich-garcia@unitec.edu>
2017-01-27 15:32:37 -0600

    Agregando ejercicio 1

--- Lab2_IlichGarcia.cpp
+++ Lab2_IlichGarcia.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <cmath>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
-// Para encontrar las soluciones 
-
+// Para ordenar el polinomio.
+int getEvaluation(int, int);
+// Para encontrar las soluciones.
 void getSolutions(int, int, int);
 
 int main() {
-	int a, b, c, option;
+	int option; // Para ejecutar los ejercicios.
+	int power, evaluation; // Grado y valor a evaluar del ejercicio 1.
+   int a, b, c; // Parámetros del ejercicio 3.
 
 	do {
-		cout << "1) Ordenar polinomio. \n2) Triángulo. \n3) Encontrar soluciones.\nEjercicio a ejecutar: ";
+		cout << "1) Ordenar polinomio. \n2) Triángulo. \n3) Encontrar soluciones.\n4) Salir. \nOpción: ";
 		cin >> option;
 
 		if (option == 1) {
+			int result;
+
+			cout << "Ingrese grado principal del polinomio: ";
+			cin >> power;
+			cout << "Ingrese valor a evaluar: ";
+			cin >> evaluation;
+
+			result = getEvaluation(power + 1, evaluation);
+
+			cout << "El resultado de evaluar " << evaluation << " en la función es: " << result << endl;
 		} else if (option == 2) {
 		} else if (option == 3) {
 			cout << "Ingrese valor de a: ";
@@ -29,6 +44,76 @@ int main() {
 	} while (option != 4);
 
 	return 0;
+}
+
+int getEvaluation(int power, int evaluation) {
+	string ascendant, descendant;
+	int result = 0, coefficient, value;
+	int values[power];
+	std::stringstream myAscendant;
+	std::stringstream myDescendant;
+
+	value = power - 1;
+
+	for (int i = 0; i < power; i++) { // Ingresando los valores al arreglo
+		cout << "Ingrese coeficiente de x^" << value << ": ";
+		cin >> coefficient;
+		
+		values[i] = coefficient;
+		value--;
+	}
+
+	value = power - 1;
+
+	for (int i = 0; i < power; i++) { // Para mostrar los valores descendentes.
+		if (i == power - 1) {
+         myDescendant << values[i];
+      } else if (values[i]  == -1) {
+         myDescendant << "-x^" << value;
+      } else if (values[i] == 1) {
+			myDescendant << "x^" << value;
+		} else if (values[i] == 0) {
+		} else {
+			if (values[i] > 0) {
+				myDescendant << "+" << values[i] << "x^" << value;
+			} else {
+				myDescendant << "-" << values[i] << "x^" << value;
+			}
+		}
+
+		value--;
+	}
+
+	descendant = myDescendant.str();
+	value = 1;
+
+/*	for (int i = 0; i < power; i++) { // Para mostrar los valores ascendentes.
+      if (i == power - 1) {
+         myAscendant << coefficient;
+      } else if (coefficient == -1) {
+         descendant << "-x^" << value;
+      }
+
+		value++;
+	} */
+
+	value = power - 1;
+
+	for (int i = 0; i < power; i++) { // Para evaluar en f(x).
+		if (value > 0) {
+			result += pow(evaluation, value) * values[i];
+		} else {
+			cout << "HOLA";
+			result += values[i];
+		}
+
+		value--;
+	}
+
+	cout << "Ascendente: " << ascendant << endl; // Imprimiendo la función ascendente.
+	cout << "Descendente: " << descendant << endl; // Imprimiendo la función descendente.
+
+	return result;
 }
 
 void getSolutions(int a, int b, int c) {
