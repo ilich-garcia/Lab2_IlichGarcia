d2d8b023d72285a4501b195d386fca1bb347d14f
Ilich García <ilich-garcia@unitec.edu>
2017-01-27 16:14:18 -0600

    Terminando ejercicio 1

--- Lab2_IlichGarcia.cpp
+++ Lab2_IlichGarcia.cpp
@@ -9,10 +9,13 @@ using namespace std;
 int getEvaluation(int, int);
 // Para encontrar las soluciones.
 void getSolutions(int, int, int);
+// Para saber si se puede formar un triángulo.
+void getTriangle(float, float, float);
 
 int main() {
 	int option; // Para ejecutar los ejercicios.
 	int power, evaluation; // Grado y valor a evaluar del ejercicio 1.
+	float first, second, third;
    int a, b, c; // Parámetros del ejercicio 3.
 
 	do {
@@ -29,8 +32,17 @@ int main() {
 
 			result = getEvaluation(power + 1, evaluation);
 
-			cout << "El resultado de evaluar " << evaluation << " en la función es: " << result << endl;
+			cout << "El resultado de evaluar " << evaluation << " en la función es: " << result << "\n" << endl;
 		} else if (option == 2) {
+			cout << "Ingrese valor de a: ";
+         cin >> first;
+         cout << "Ingrese valor de b: ";
+         cin >> second;
+         cout << "Ingrese valor de c: ";
+         cin >> third;
+
+			cout << "Se puede construir?: ";
+			getTriangle(first, second, third);
 		} else if (option == 3) {
 			cout << "Ingrese valor de a: ";
 		 	cin >> a;
@@ -67,17 +79,23 @@ int getEvaluation(int power, int evaluation) {
 
 	for (int i = 0; i < power; i++) { // Para mostrar los valores descendentes.
 		if (i == power - 1) {
-         myDescendant << values[i];
+			if (values[i] > 0) {
+				myDescendant << "+" << values[i];
+			} else {
+	         myDescendant << values[i];
+			}
       } else if (values[i]  == -1) {
          myDescendant << "-x^" << value;
       } else if (values[i] == 1) {
 			myDescendant << "x^" << value;
 		} else if (values[i] == 0) {
+		} else if (i == 0) {
+			myDescendant << values[i] << "x^" << value;
 		} else {
 			if (values[i] > 0) {
 				myDescendant << "+" << values[i] << "x^" << value;
 			} else {
-				myDescendant << "-" << values[i] << "x^" << value;
+				myDescendant << values[i] << "x^" << value;
 			}
 		}
 
@@ -85,25 +103,35 @@ int getEvaluation(int power, int evaluation) {
 	}
 
 	descendant = myDescendant.str();
-	value = 1;
+	value = 0;
 
-/*	for (int i = 0; i < power; i++) { // Para mostrar los valores ascendentes.
-      if (i == power - 1) {
-         myAscendant << coefficient;
-      } else if (coefficient == -1) {
-         descendant << "-x^" << value;
+	for (int i = 0; i < power; i++) { // Para mostrar los valores ascendentes.
+      if (i == 0) {
+         myAscendant << values[i];
+      } else if (values[i] == -1) {
+         myAscendant << "-x^" << value;
+      } else if (values[i] == 1) {
+			myAscendant << "x^" << value;
+		} else if (values[i] == 0) {
+		} else {
+         if (values[i] > 0) {
+            myAscendant << "+" << values[i] << "x^" << value;
+         } else {
+            myAscendant << values[i] << "x^" << value;
+         }
       }
 
-		value++;
-	} */
 
+		value++;
+	}
+
+	ascendant = myAscendant.str();
 	value = power - 1;
 
 	for (int i = 0; i < power; i++) { // Para evaluar en f(x).
 		if (value > 0) {
 			result += pow(evaluation, value) * values[i];
 		} else {
-			cout << "HOLA";
 			result += values[i];
 		}
 
@@ -111,9 +139,23 @@ int getEvaluation(int power, int evaluation) {
 	}
 
 	cout << "Ascendente: " << ascendant << endl; // Imprimiendo la función ascendente.
-	cout << "Descendente: " << descendant << endl; // Imprimiendo la función descendente.
+	cout << "Descendente: " << descendant << "\n" << endl; // Imprimiendo la función descendente.
 
 	return result;
+}
+
+void getTriangle(float first, float second, float third) {
+	bool canGet = false;
+
+	if (first + second > third	&& first + third > second && second + third > first) {
+		canGet = true;
+	}
+
+	if (canGet) {
+		cout << "Sí!!\n" << endl;
+	} else {
+		cout << "Neles pasteles!!\n" << endl;
+	}
 }
 
 void getSolutions(int a, int b, int c) {
@@ -126,8 +168,8 @@ void getSolutions(int a, int b, int c) {
 	} else if (determinant < 0) {
 		cout << "No tiene soluciones en los Reales... Use la imaginación";
 	} else {
-		solution1 = (-b + sqrt(determinant)) / 2;
-		solution2 = (-b - sqrt(determinant)) / 2;
+		solution1 = float((-b + sqrt(determinant)) / (2 * a));
+		solution2 = float((-b - sqrt(determinant)) / (2 * a));
 
 		cout << "Soluciones: x1 = " << solution1 << ", x2 = " << solution2 << endl;
 	}
